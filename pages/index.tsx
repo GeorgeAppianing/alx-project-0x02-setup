d311f968a52385073756032d931daed4f44376d3
GeorgeAppianing <george.adu.appianing@gmail.com>
2025-11-17 11:16:14 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
-export interface ExampleInterface {
-  message: string;
+import Header from "@/components/layout/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to ALX Project Setup 🚀
+        </h1>
+      </main>
+    </>
+  );
 }
