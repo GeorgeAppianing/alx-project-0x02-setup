afd1a9485215f9e0c3460876cb2cfdab40f1d911
GeorgeAppianing <george.adu.appianing@gmail.com>
2025-11-17 11:14:07 +0000

    Update Header.tsx

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,11 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full bg-gray-900 text-white p-4 flex gap-6">
-      <Link href="/home">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/posts">Posts</Link>
-      <Link href="/users">Users</Link>
+    <header className="w-full bg-gray-800 text-white p-4 flex gap-6">
+      <Link href="/">Home</Link>
     </header>
   );
 }
